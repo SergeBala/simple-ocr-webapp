6027717899ca4282b0381f280348b271386ef2b2
Sergey Balashov <balashevski@gmail.com>
2024-06-17 15:00:22 +0400

    fixed service 2: requests seem to work

--- em_app/main.py
+++ em_app/main.py
@@ -119,6 +119,12 @@ if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8800)
 
 
+
+
+
+
+
+
 # from fastapi import FastAPI, Depends, HTTPException, UploadFile
 # from sqlalchemy.orm import Session
 # from em_app.database import SessionLocal, engine, Base
--- em_app/service2.py
+++ em_app/service2.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import random
 import pytesseract
 from PIL import Image
 import io
 
+#this is just a placeholder little way of actually processing the image, just for fun! it only works on screenshots of text
 def extract_text_from_image(file_bytes: bytes) -> str:
     image = Image.open(io.BytesIO(file_bytes))
     text = pytesseract.image_to_string(image)
+    if text and text[-1] == '/n':
+        text = text[:-1]
     return text
 
 app = FastAPI()
@@ -14,8 +17,14 @@ app = FastAPI()
 @app.post("/process-image/")
 async def process_image(file: UploadFile = File(...)):
     # Simulate image processing and generate a numeric employee ID
-    employee_id = extract_text_from_image()
-    if not (employee_id.isdigit() and len(s) == 3): 
+
+    try:
+        file_bytes = await file.read()
+        employee_id  = extract_text_from_image(file_bytes)
+        print(f"Here's what service2 managed to read from the id photo:{employee_id}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    if not (employee_id.isdigit() and len(employee_id) == 3):
         employee_id = f"{random.randint(100, 999)}"
     return {"employee_id": employee_id}
 
