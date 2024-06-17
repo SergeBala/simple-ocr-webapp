ed41a339b76f3406cc43f6befedf93aee760b7ec
Sergey Balashov <balashevski@gmail.com>
2024-06-17 15:29:51 +0400

    minor fix get emp by id, not working yet

--- em_app/crud.py
+++ em_app/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from em_app import models, schemas
 
 def get_employee(db: Session, employee_id: str):
-    return db.query(models.Employee).filter(models.Employee.id == employee_id).first()
+    return db.query(models.Employee).filter(models.Employee.employee_id == employee_id).first()
 
 def get_employees(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.Employee).offset(skip).limit(limit).all()
